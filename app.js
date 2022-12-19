26bc349ff4e15867a97f66f922b111e16398a9a5
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2022-12-18 23:47:53 -0500

    Various bugfixes
    Normal game execution no longer mentions a value of 'undefined'.
    
    With regard to game play: we don't handle ties yet.
    We plan on fixing this next.

--- app.js
+++ app.js
@@ -17,16 +17,17 @@ const playerKey = { rock: 0, paper: 1, scissors: 2 };
 // Return 'true' if the player wins.
 function playerWins (computerChoice, playerChoice) {
     return computerChoice - playerChoice === 2 // computer: scissors; player: rock
-	|| computerChoice > playerChoice; // any other play
+	|| computerChoice < playerChoice; // any other play
 }
 
-function playRound (pname) {
+// Return a string indicating the winner.
+// Don't interpret top-level user-input yet.
+function playRound (playerChoice) {
     const computerChoice = Math.floor(Math.random() * 3);
-    const playerChoice = playerKey[pname];
-
-    const playerWon = playerWins(playerChoice, computerChoice);
+    const playerWon = playerWins(computerChoice, playerChoice);
 
     const cname = gameKey[computerChoice];
+    const pname = gameKey[playerChoice];
 
     if (playerWon) {
 	return `You win! ${pname} beats ${cname}`;
@@ -35,20 +36,32 @@ function playRound (pname) {
     return `You lose: ${cname} beats ${pname}`;
 }
 
+function getInput (roundNumber) {
+    while (true) {
+	const playerInput = prompt(`Your move for round ${roundNumber}`);
+
+	if (!playerInput) {
+	    alert("bye!");
+	}
+
+	if (gameKey.includes(playerInput.toLowerCase())) {
+	    return playerInput;
+	}
+
+	alert("Invalid choice: try again");
+    }
+}
+
 function game () {
     let playerScore = 0;
     let computerScore = 0;
 
     for (let i = 0; i < 5; i++) {
-	const playerInput = prompt(`Your move for round ${i + 1}`);
+	const playerInput = getInput(i + 1);
+	const playerInteger = playerKey[playerInput];
 
-	if (!playerInput) {
-	    alert("bye!");
-	    break;
-	}
+	console.assert(typeof playerInteger === "number" && 0 <= playerInteger && playerInteger <= 2);
 
-	alert(playerInput);
+	alert(playRound(playerInteger));
     }
 }
-
-game();
