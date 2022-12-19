cabbefdf7be6675504c7ac7759257621f63ebc5e
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2022-12-18 23:07:38 -0500

    Test simple inputting by user

--- app.js
+++ app.js
@@ -10,14 +10,45 @@ smaller number. But there is one twist. If the difference between the
 two numbers is 2, the smaller of the two numbers wins.
 */
 
+// Game key
+const gameKey = ["rock", "paper", "scissors"];
+const playerKey = { rock: 0, paper: 1, scissors: 2 };
 
 // Return 'true' if the player wins.
-function playerWins(computerChoice, playerChoice) {
+function playerWins (computerChoice, playerChoice) {
     return computerChoice - playerChoice === 2 // computer: scissors; player: rock
 	|| computerChoice > playerChoice; // any other play
 }
 
+function playRound (pname) {
+    const computerChoice = Math.floor(Math.random() * 3);
+    const playerChoice = playerKey[pname];
 
-function getComputerChoice () {
-    return Math.floor(Math.random() * 3);
+    const playerWon = playerWins(playerChoice, computerChoice);
+
+    const cname = gameKey[computerChoice];
+
+    if (playerWon) {
+	return `You win! ${pname} beats ${cname}`;
+    }
+
+    return `You lose: ${cname} beats ${pname}`;
 }
+
+function game () {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+	const playerInput = prompt(`Your move for round ${i + 1}`);
+
+	if (!playerInput) {
+	    alert("bye!");
+	    break;
+	}
+
+	alert(playerInput);
+    }
+}
+
+game();
